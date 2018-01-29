f25f03cb51656dcd57cdec37c657695c17933faf
lishengpeng <211789390@qq.com>
2018-01-29 10:05:29 +0800

    ready

--- Yangzixu_1998/readme.md
+++
@@ -1,3 +0,0 @@
-Hello everybody,
-  it's a great honor for me to do and share some OJ_Tests with you.
-Our common goal is Ac!
--- 李胜鹏/poj1001.cpp
+++
@@ -1,9 +0,0 @@
-//Time : Mon Jan 08 12:28:17 2018
-//Author : Lishengpeng
-#include <iostream>
-using namespace std;
-int main(int argc, char const *argv[])
-{
-
-	return 0;
-}
